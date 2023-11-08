4b2fdd541dc02393db7dd33c230f84b34b4f7359
Mohammed Faisal <mohamedfaisl1000@gmail.com>
2023-11-07 20:02:44 -0800

    str_fun

---
+++ str_fun2.c
@@ -0,0 +1,83 @@
+#include "shell.h"
+/**
+ * _strcpy - copies a string
+ * @dest: the destination
+ * @src: the source
+ *
+ * Return: pointer to destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+/**
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string
+ */
+
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *result;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	result = malloc(sizeof(char) * (length + 1));
+	if (!result)
+		return (NULL);
+	for (lenght++; length--;)
+		result[length] = *--str;
+	return (result);
+}
+/**
+ * _puts - prints an input string
+ * @str: the string to be printed
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: on success 1, on error -1, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buff[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
